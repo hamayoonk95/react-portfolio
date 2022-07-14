11e7fabaad51591f13746c1a55efed096c1242b1
Hamayoon <hamayoonk38@gmail.com>
2022-07-14 12:50:37 +0100

    finished about section

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,20 +1,28 @@
 import logo from '../assets/logo.png';
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
-import redux from '../assets/redux.png';
+import bootstrap_icon from '../assets/bootstrap.png';
 import JS from '../assets/JS.png';
-import flutter from '../assets/flutter.png';
-import sass from '../assets/sass.png';
+import github_icon from '../assets/github.png';
+import react_icon from '../assets/react.png';
 import bgImg from '../assets/bgIMG.png';
+import about_1 from '../assets/about01.png';
+import about_2 from '../assets/about02.png';
+import about_3 from '../assets/about03.png';
+import about_4 from '../assets/about04.png';
 
 
 export default {
     logo,
     profile,
     circle,
-    redux,
+    bootstrap_icon,
     JS,
-    flutter,
-    sass,
-    bgImg
+    github_icon,
+    react_icon,
+    bgImg,
+    about_1,
+    about_2,
+    about_3,
+    about_4
 };
